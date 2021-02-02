1838da49ec89c454f203e973f6a56a806455eb81
Guilherme A.V. Marques <guilhermevidal4857@gmail.com>
2021-02-02 20:45:58 -0300

    Admin register a promotion finished

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -5,4 +5,12 @@ class PromotionsController < ApplicationController
   def show
     @promotion = Promotion.find(params[:id])
   end
+  def new
+  end
+  def create
+    @promotion = Promotion.new(params.require(:promotion).permit(:name, 
+                 :description, :code, :discount_rate, :coupon_quantity, :expiration_date))
+    @promotion.save
+    redirect_to promotion_path(@promotion)
+  end
 end
