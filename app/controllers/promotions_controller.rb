36821caa1726c974b90fd350362d659a4c9d338b
Guilherme A.V. Marques <guilhermevidal4857@gmail.com>
2021-02-04 08:32:41 -0300

    Promotion validation finished

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -2,15 +2,22 @@ class PromotionsController < ApplicationController
   def index
     @promotions = Promotion.all
   end
+  
   def show
     @promotion = Promotion.find(params[:id])
   end
+
   def new
+    @promotion = Promotion.new
   end
+
   def create
-    @promotion = Promotion.new(params.require(:promotion).permit(:name, 
-                 :description, :code, :discount_rate, :coupon_quantity, :expiration_date))
-    @promotion.save
-    redirect_to promotion_path(@promotion)
+    @promotion = Promotion.new(params.require(:promotion).permit(:name, :description, :code, 
+                                                                 :discount_rate, :coupon_quantity, :expiration_date))
+    if @promotion.save
+      redirect_to @promotion
+    else
+      render 'new'
+    end
   end
 end
